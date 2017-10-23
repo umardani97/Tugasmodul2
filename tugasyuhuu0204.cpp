5915a1263d822bbe34de47f06bf679dfd232ee66
umardani97 <umardan077@gmail.com>
2017-10-24 03:58:50 +0800

    mencari luas 1 box parket

--- tugasyuhuu0204.cpp
+++ tugasyuhuu0204.cpp
@@ -3,9 +3,20 @@ using namespace std;
 
 int main(){
 	int luasrumah=42;
-	cout<<"Luas Rumah         : 42m2";
+	cout<<"Luas Rumah         : 42m2"<<"\n";
+		
+	double panjangparket=0.193;
+	cout<<"Panjang Parket     : "<<panjangparket<<"m2\n";
 	
-	double luas1boxparket=1.87596;
-	cout<<"Luas 1 Box Parket  : "<<luas1boxparket<<"/n";
+	double lebarparket=1.215;
+	cout<<"Lebar Parket       : "<<lebarparket<<"m2\n";
+	
+	double kepingparket=panjangparket*lebarparket;
+	cout<<"1 Keping Parket    : "<<kepingparket<<"m2\n";
+	cout<<"1 box              : 8 keping parket\n";
+	
+	double luas1boxparket=kepingparket*8;
+	cout<<"Luas 1 box Parket  : "<<luas1boxparket<<"m2\n";
+	
 	
 }
