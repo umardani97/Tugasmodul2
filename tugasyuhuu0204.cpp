ae3f4ee9facc348515eb6ad562c6bc5e7a36e0d9
umardani97 <umardan077@gmail.com>
2017-10-24 03:41:48 +0800

    diketahui luas 1 box parket

--- tugasyuhuu0204.cpp
+++ tugasyuhuu0204.cpp
@@ -3,6 +3,9 @@ using namespace std;
 
 int main(){
 	int luasrumah=42;
-	cout<<"Luas Rumah   :42m2";
+	cout<<"Luas Rumah         : 42m2";
+	
+	double luas1boxparket=1.87596;
+	cout<<"Luas 1 Box Parket  : "<<luas1boxparket<<"/n";
 	
 }
